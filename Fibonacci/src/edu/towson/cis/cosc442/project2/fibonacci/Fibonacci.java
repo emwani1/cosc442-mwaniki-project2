73a58c768986d02e8360669b1e18694b2246941c
emwani1 <edwinmwaniki@Edwins-MBP-2.home>
2017-02-23 21:31:18 -0500

    changed case :0 to return 0

--- Fibonacci/src/edu/towson/cis/cosc442/project2/fibonacci/Fibonacci.java
+++ Fibonacci/src/edu/towson/cis/cosc442/project2/fibonacci/Fibonacci.java
@@ -14,9 +14,9 @@ public class Fibonacci {
 	 */
 	public int fibonacci(int n) {
 		switch (n) {
-			case 0: return 1;
-			case 1: return 1;
-			default: return (fibonacci(n - 1) + fibonacci(n - 2));
+		case 0: return 0;
+		case 1: return 1;
+		default: return (fibonacci(n - 1) + fibonacci(n - 2));
 		}
 	}
 }
